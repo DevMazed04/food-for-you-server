360881759e1275dde643f6b25337bd843b8e216a
DevMazed04 <mdmazedislam2684@gmail.com>
2022-11-09 22:25:30 +0600

    created a root api and tested the server

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
+const app = express();
 const port = process.env.PORT || 5000;
 
 //middlewares
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+   res.send("Food For You API is running");
+});
+
+app.listen(port, () => {
+   console.log("Food For You Server is running on port", port);
+});
