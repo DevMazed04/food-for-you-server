90422b444b2bc3510d7ec89639d8befb441aa0bc
DevMazed04 <mdmazedislam2684@gmail.com>
2022-11-10 01:14:48 +0600

    inserted services data to foodForYou database in mongodb

--- index.js
+++ index.js
@@ -13,13 +13,14 @@ app.use(express.json());
 //database connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vfwjfcr.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-   const collection = client.db("test").collection("devices");
-   // perform actions on the collection object
-   client.close();
-});
 
-
+const run = async () => {
+   try {
+      const serviceCollection = client.db("foodForYou").collection("services");
+   } finally {
+   }
+};
+run().catch((err) => console.log(err));
 
 
 
