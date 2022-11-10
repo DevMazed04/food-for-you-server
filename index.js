5abbc5e341fd3db8dbadb16ae85dd0dc0150b1d7
DevMazed04 <mdmazedislam2684@gmail.com>
2022-11-10 17:03:39 +0600

    created services and services/:id get api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -14,19 +14,42 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vfwjfcr.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+app.get("/", (req, res) => {
+   res.send("Food For You API is running");
+});
+
 const run = async () => {
    try {
       const serviceCollection = client.db("foodForYou").collection("services");
+      const reviewCollection = client.db("foodForYou").collection("reviews");
+
+      app.get('/services', async (req, res) => {
+         const query = {};
+         const cursor = serviceCollection.find(query);
+         const services = await cursor.toArray();
+         res.send(services);
+      })
+
+
+      app.get('/services/:id', async (req, res) => {
+         const id = req.params.id;
+         const query = { _id: ObjectId(id) };
+         const service = await serviceCollection.findOne(query);
+         res.send(service);
+      })
+
+      //reviews api
+
+      app.post("/reviews", async (req, res) => {
+         const review = req.body;
+         const result = await reviewCollection.insertOne(review);
+         console.log(result);
+         res.send(result);
+      });
    } finally {
    }
 };
 run().catch((err) => console.log(err));
-
-
-
-app.get("/", (req, res) => {
-   res.send("Food For You API is running");
-});
 
 app.listen(port, () => {
    console.log("Food For You Server is running on port", port);
