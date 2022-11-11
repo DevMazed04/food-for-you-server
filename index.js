afca43f0a90b1cdf24db4ee7d10ff86453041549
DevMazed04 <mdmazedislam2684@gmail.com>
2022-11-11 14:27:03 +0600

    created delete api using delete method

--- index.js
+++ index.js
@@ -39,14 +39,32 @@ const run = async () => {
       })
 
       //reviews api
+      app.get('/reviews', async (req, res) => {
+         let query = {};
+         if (req.query.email) {
+            query = { email: req.query.email }
+         }
+         const cursor = reviewCollection.find(query);
+         const reviews = await cursor.toArray();
+         res.send(reviews);
+      })
 
       app.post("/reviews", async (req, res) => {
          const review = req.body;
          const result = await reviewCollection.insertOne(review);
-         console.log(result);
+         // console.log(result);
          res.send(result);
       });
-   } finally {
+
+
+      app.delete("/reviews/id:", async (req, res) => {
+         const id = req.params.id;
+         const query = { _id: ObjectId(id) };
+         const result = await reviewCollection.deleteOne(query);
+         res.send(result);
+      });
+   }
+   finally {
    }
 };
 run().catch((err) => console.log(err));
